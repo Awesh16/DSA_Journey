d1fb8a124a3d1c8c027a6d596a42a93f8b4f172d
Md Awesh Raza Quadri <mdawesh1608@gmail.com>
2025-08-31 21:57:28 +0530

    Time: 2089 ms (20.15%), Space: 7.9 MB (6.94%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        if(num<=0)
+        return false;
+        int i,s=0;
+        for(i=1;i<num;i++)
+        {
+            if(num%i==0)
+            s=s+i;
+        }if(s==num)
+        return true;
+        else 
+        return false;
+    }
+};
