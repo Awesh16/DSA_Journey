af451829325f834a077e8d5cba25cf355e006e51
Md Awesh Raza Quadri <mdawesh1608@gmail.com>
2025-08-31 21:44:39 +0530

    Time: 0 ms (100%), Space: 7.9 MB (45%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        if(n<=0)
+        return false;
+   int primes[]={2,3,5};
+   for(auto i:primes)
+   {
+    while(n%i==0)
+    {
+        n/=i;
+    }
+   }
+   return n==1;
+    
+    }
+};
