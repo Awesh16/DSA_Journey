876f2577613d7defd7e1f7920ee016905579c16d
Md Awesh Raza Quadri <mdawesh1608@gmail.com>
2025-09-05 12:39:21 +0530

    Time: 7 ms (11.51%), Space: 28 MB (86.73%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,19 +1,9 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-       unordered_map<int,int>hash;
-        int i,k=0,l;
-        for(i=0;i<nums.size();i++)
-        {
-            hash[nums[i]]++;
-        }
-        for(i=0;i<nums.size();i++)
-        {if(hash[nums[i]]>k)
-        {
-            k=hash[nums[i]];
-            l=i;
-        }}
-        return nums[l];
+       sort(nums.begin(),nums. end());
+      return(nums[nums.size()/2]);
+        
 
         }
     
